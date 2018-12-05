cd890188b726f94453d42e3b0838335291e32298
projetositegsm <projeto.site.gsm@gmail.com>
2018-12-05 21:05:23 -0200

    Small adjust

--- templates/js/scroll.js
+++ templates/js/scroll.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
       var hash = this.hash;
 
       $('html, body').animate({
-        scrollTop: $(hash).offset().top - 77
+        scrollTop: $(hash).offset().top - 85
       }, 800, function(){
 
         window.location.hash = hash;
